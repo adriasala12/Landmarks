c76c76e2834d3cabd89f6ea7008aa160eda21b4a
Adrià Sala <adriasala124@gmail.com>
2020-10-03 21:07:12 +0200

    Text modification

--- Landmarks/ContentView.swift
+++ Landmarks/ContentView.swift
@@ -9,8 +9,10 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, Barcelona!")
+        Text("Turtle Rock")
+            .font(.title)
             .padding()
+            
     }
 }
 
