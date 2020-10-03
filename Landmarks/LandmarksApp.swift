db644ac3d92edc173a80c59fbfb149603b096d56
Adrià Sala <adriasala124@gmail.com>
2020-10-03 12:43:08 +0200

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Adrià Sala Roget on 03/10/2020.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
